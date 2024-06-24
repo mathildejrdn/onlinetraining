8baaa2a8e807fa9a3e2a18927ec15f1beec8db3f
m.jourden <74381521+mathildejrdn@users.noreply.github.com>
2024-06-24 16:19:32 +0200

    planifictation et structure du back end avec liens sur l'index pour la période de développement

--- src/index.php
+++ src/index.php
@@ -10,13 +10,30 @@
 </head>
 <body>
 
-  <!-- Your content here -->
+  
+  <a href="../back_office/add_category.php">Ajouter une catégorie (super amdin)</a>
+  <a href="../back_office/userlist.php">Liste des users (super admin)</a> <br>
+  <a href="../back_office/inbox.php">Messagerie</a>
+  <a href="../back_office/orders.php">Commades</a>
+  <a href="../back_office/edit_orders.php">Editer une commade</a>
+  <a href="../back_office/products.php">Produits</a>
+  <a href="../back_office/add_product.php">Ajouter un produit</a>
+  <a href="../back_office/add_category.php">Editer un produit</a>
+  <br>
+  <a href="../client_side/contact.php">Formulaire de contact</a>
+  <a href="../client_side/login.php">Formulaire de connection</a>
+  <a href="../client_side/signup.php">Formulaire d'inscription</a>
+  <a href="../client_side/panier.php">panier</a>
 
+  <!-- Your content here -->
+<!-- <br>
+Le front ici
   <?php 
     include 'includes/nav.php';
     include 'includes/garanties.php';
     include 'includes/newsletter.php';
-    include 'includes/footer.php'; ?>
+    include 'includes/footer.php'; 
+  ?> -->
 
 </body>
 </html>

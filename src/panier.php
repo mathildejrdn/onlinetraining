c0237d42485d34b069aa567dae7f6f4d10ee18a4
leaf2606 <fort.lea26@gmail.com>
2024-06-26 16:58:17 +0200

    modif panier

--- src/panier.php
+++ src/panier.php
@@ -19,6 +19,7 @@ function ajouterAuPanier($id, $nomProduit, $quantite, $prix) {
 
     if (isset($_SESSION['panier'][$id])) {
         $_SESSION['panier'][$id]['quantite'] += $quantite;
+        $_SESSION['panier'][$id]['prix_unitaire'] += $prix;
         $_SESSION['panier'][$id]['prix_total'] += $prix * $quantite;
     } else {
         $_SESSION['panier'][$id] = array(
@@ -37,7 +38,7 @@ function afficherPanier() {
         echo "Votre panier est vide.";
     } else {
         echo "<table border='1'>";
-        echo "<tr><th>Nom du produit</th><th>Quantité</th><th>Prix Unitaire</th><th>Prix Total</th><th>Action</th></tr>";
+        echo "<tr><th>Nom du produit</th><th>Quantité</th><th>Prix unitaire</th><th>Prix total</th><th>Action</th></tr>";
         foreach ($_SESSION['panier'] as $id => $produit) {
             echo "<tr>";
             echo "<td>{$produit['nomProduit']}</td>";
@@ -83,8 +84,8 @@ function supprimerDuPanier($id) {
 if (isset($_GET['action'])) {
     switch ($_GET['action']) {
         case 'add':
-            if (isset($_GET['id']) && isset($_GET['nomProduit']) && isset($_GET['quantite'])) {
-                ajouterAuPanier($_GET['id'], $_GET['nomProduit'], $_GET['quantite']);
+            if (isset($_GET['id']) && isset($_GET['nomProduit']) && isset($_GET['quantite']) && isset($_GET['prix'])) {
+                ajouterAuPanier($_GET['id'], $_GET['nomProduit'], $_GET['quantite'], $_GET['quantite'], $_GET['prix']);
             }
             break;
         case 'update':
@@ -117,9 +118,7 @@ afficherPanier();
 
 <body>
 
-    <a href="panier.php?action=add&id=ID_DU_PRODUIT&nomProduit=NomDuProduit&quantite=1">Ajouter un produit</a>
-
-    <a href="panier.php?action=id=1">Retourner à la boutique</a>
+    <a href="panier.php?action=add&id=ID_DU_PRODUIT&nomProduit=NomDuProduit&quantite&prix=1">Retourner à la boutique</a>
 
 </body>
 

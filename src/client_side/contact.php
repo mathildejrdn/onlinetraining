fabebe85a8231e52d8804da5ad1b21c599de7d29
m.jourden <74381521+mathildejrdn@users.noreply.github.com>
2024-06-26 16:52:50 +0200

    test de formulaire de contact

--- src/client_side/contact.php
+++ src/client_side/contact.php
@@ -1,1 +1,36 @@
-formulaire de contact
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de Contact</title>
+    <link rel="stylesheet" href="../styles/output.css">
+    <link rel="stylesheet" href="../styles/reset.css">
+</head>
+<body class="bg-gray-100">
+
+    <div class="flex items-center justify-center min-h-screen">
+
+    <form class="max-w-md  bg-white p-6 rounded shadow-md w-full" method="post">
+        <h1 class="text-2xl font-bold mb-4">Connexion</h1>
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="email" name="email" id="email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer" placeholder=" " required />
+            <label for="email" class="peer-focus:font-medium absolute text-sm text-red-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-red-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
+        </div>
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="password" name="pass" id="pass" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer" placeholder=" " required />
+            <label for="pass" class="peer-focus:font-medium absolute text-sm text-red-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-red-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Mot de passe</label>
+        </div>
+        <div id="bouton">
+            <button type="submit" class="w-full bg-red-500 text-white py-2 rounded hover:bg-red-600">Me connecter</button>
+        </div>
+        <div id="inscription" class="mt-4 text-center">
+        <a href="signup.php" class="text-red-700 hover:underline">Cliquez ici si vous n'êtes pas enregistré</a>
+    </div>  
+    </form>
+   <!-- Image de fond à droite pour les écrans md et plus grands -->
+   <img src='../images/imagetest.png' alt="madame avec des sacs" class="hidden md:block w-1/2 h-1/2 bg-cover bg-no-repeat bg-right">
+</img>
+    </div>
+</body>
+</html>

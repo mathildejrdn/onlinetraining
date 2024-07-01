8c20b73e9429ce58d85b5f9b6dbb79f7381c98bd
m.jourden <74381521+mathildejrdn@users.noreply.github.com>
2024-07-01 09:53:56 +0200

    gestion des stocks

--- src/back_office/stock_discard.php
+++ src/back_office/stock_discard.php
@@ -29,9 +29,7 @@
         <table class="w-full text-sm text-left text-gray-500">
           <thead class="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
-              <th scope="col" class="px-6 py-3">Numéro de commande</th>
-              <th scope="col" class="px-6 py-3">Date de la commande</th>
-              <th scope="col" class="px-6 py-3">Date de clôture</th>
+              <th scope="col" class="px-6 py-3">Référence de l'article</th>
               <th scope="col" class="px-6 py-3">Raison de la suppression</th>
               <th scope="col" class="px-6 py-3">Apprenant(e)</th>
               <th scope="col" class="px-6 py-3">Actions</th>
@@ -40,8 +38,6 @@
           <tbody>
             <tr class="bg-white border-b hover:bg-gray-50">
               <td class="px-6 py-4">CMD001</td>
-              <td class="px-6 py-4">2023-06-15</td>
-              <td class="px-6 py-4">-</td>
               <td class="px-6 py-4">Produit endommagé</td>
               <td class="px-6 py-4">John Doe</td>
               <td class="flex items-center px-6 py-4 space-x-2">
@@ -50,12 +46,6 @@
                     <path d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"/>
                   </svg>
                   <span>Détails</span>
-                </a>
-                <a href="order_archive.php?id=1" class="font-medium text-green-600 hover:underline flex items-center space-x-1">
-                  <svg class="w-6 h-6 text-green-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-6 7 2 2 4-4m-5-9v4h4V3h-4Z"/>
-                  </svg>
-                  <span>Désarchiver </span>
                 </a>
                 <a href="delete_order.php?id=1" class="font-medium text-red-600 hover:underline flex items-center space-x-1">
                   <svg class="w-6 h-6 text-red-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -67,8 +57,6 @@
             </tr>
             <tr class="bg-white border-b hover:bg-gray-50">
               <td class="px-6 py-4">CMD002</td>
-              <td class="px-6 py-4">2023-06-16</td>
-              <td class="px-6 py-4">-</td>
               <td class="px-6 py-4">Taille non correspondante</td>
               <td class="px-6 py-4">Jane Smith</td>
               <td class="flex items-center px-6 py-4 space-x-2">
@@ -77,12 +65,6 @@
                     <path d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"/>
                   </svg>
                   <span>Détails</span>
-                </a>
-                <a href="order_archive.php?id=2" class="font-medium text-green-600 hover:underline flex items-center space-x-1">
-                  <svg class="w-6 h-6 text-green-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-6 7 2 2 4-4m-5-9v4h4V3h-4Z"/>
-                  </svg>
-                  <span>Désarchiver </span>
                 </a>
                 <a href="delete_order.php?id=2" class="font-medium text-red-600 hover:underline flex items-center space-x-1">
                   <svg class="w-6 h-6 text-red-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">

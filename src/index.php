beadf48097130c1ff4e2bdcce1de4bb8149f38c2
m.jourden <74381521+mathildejrdn@users.noreply.github.com>
2024-07-02 16:10:12 +0200

    index homme femme

--- src/index.php
+++ src/index.php
@@ -25,8 +25,11 @@ include 'includes/header.php';
 
 <!-- Affichage des images pour hommes et femmes -->
 <div class="container">
-  <img src="./images/femmes.jpg" alt="femme">
+  <img src="./images/femme.jpg" alt="femme">
+  <!-- ajouter l'effet de hover avec mode femme et mode homme -->
   <img src="./images/homme.jpg" alt="homme">
+  <img src="./images/beaute.jpg" alt="beaute">
+  <!-- les produits de beauté arrivent bientôt comme texte en hover -->
 </div>
 
 <!-- Affichage des produits récupérés depuis la base de données -->

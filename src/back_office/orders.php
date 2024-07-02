9687a5530f29d9af085d7f9cb0d823c26724e848
m.jourden <74381521+mathildejrdn@users.noreply.github.com>
2024-07-02 12:28:21 +0200

    tableau des commandes

--- src/back_office/orders.php
+++ src/back_office/orders.php
@@ -55,7 +55,7 @@
                   <svg class="w-6 h-6 text-blue-600 hover:text-blue-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"/>
                   </svg>
-                  <span>Staut de la commande</span>
+                  <span>Détails</span>
                 </a>
                 <a href="orders_finished.php" class="font-medium text-green-600 hover:underline flex items-center space-x-1">
                   <svg class="w-6 h-6 text-green-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">

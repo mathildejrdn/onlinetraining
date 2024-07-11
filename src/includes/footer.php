36532dc54a13a5b3d84553c5305f24b73f377dee
m.jourden <74381521+mathildejrdn@users.noreply.github.com>
2024-07-11 15:00:45 +0200

    front end terminé

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,37 +1,59 @@
-<footer class="rounded-lg shadow bg-gray-900 m-4">
+<footer class="rounded-lg shadow bg-gray-950 m-4">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-start sm:justify-between">
-            <div class="flex flex-wrap items-start justify-between w-full sm:w-auto">
-                <div class="mb-4 mr-8">
-                    <ul class="text-gray-400"> <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse ml-auto">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap text-gray-400">Flowbite</span>
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-            </a>
-                        <li class="mb-2">Permettre au plus grand nombre d’accéder librement, sans contrainte de lieu, de temps ou de rythme à des formations modulaires et souples.</li>
-                        <hr>
-<a href="https://www.onlineformapro.com/mentions_legales/" target="_blank" rel="noopener noreferrer">Mentions légales</a> 
-<a href="https://www.onlineformapro.com/mentions_legales_rgpd/" target="_blank" rel="noopener noreferrer">RGPD</a> 
-<a href="https://www.onlineformapro.com/cgv/" target="_blank" rel="noopener noreferrer">CGV</a> 
-<a href="https://www.onlineformapro.com/certification-qualiopi/" target="_blank" rel="noopener noreferrer" class="hover:red-500">Certification QUALILOPI</a>
-<li class="mb-2"><!-- liens et icons des réseaux sociaux d'online --></li>
-</ul>
-                </div>
-                <div class="mb-4 mr-8">
-                 <!-- colone vide -->
-                </div>
-                <div>
-                    <h2 class="mb-2 text-sm font-semibold uppercase text-gray-400">Contact</h2>
+            <div class="flex flex-wrap items-start justify-between w-full">
+                <div class="mb-4 mr-8 w-full sm:w-auto">
                     <ul class="text-gray-400">
-                        <li class="mb-2"><a href="#" class="hover:underline">adresse</a></li>
-                        <li class="mb-2"><a href="#" class="hover:underline">téléphone</a></li>
-                        <li class="mb-2"><a href="#" class="hover:underline">email</a></li>
+                        <a href="#" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse ml-auto">
+                            <img src="./images/logo_white.svg" class="h-12" alt="Online formapro Logo" />
+                        </a>
+                        <li class="mb-2">
+                            <p class="text-gray-400">Permettre au plus grand nombre d’accéder librement,<br>sans contrainte de lieu, de temps ou de rythme<br>à des formations modulaires et souples.</p>
+                        </li>
+                        <hr class="border-gray-700">
+                        <div class="flex space-x-3 mt-4">
+                            <a href="https://www.onlineformapro.com/mentions_legales/" target="_blank" rel="noopener noreferrer" class="hover:text-red-500">Mentions légales</a> 
+                            <a href="https://www.onlineformapro.com/mentions_legales_rgpd/" target="_blank" rel="noopener noreferrer" class="hover:text-red-500">RGPD</a> 
+                            <a href="https://www.onlineformapro.com/cgv/" target="_blank" rel="noopener noreferrer" class="hover:text-red-500">CGV</a> 
+                            <a href="https://www.onlineformapro.com/certification-qualiopi/" target="_blank" rel="noopener noreferrer" class="hover:text-red-500">Certification QUALILOPI</a>
+                        </div>
+                        <div class="mt-4 flex space-x-4">
+                            <!-- LinkedIn Icon -->
+                            <a href="https://www.linkedin.com/company/onlineformapro/" target="_blank" rel="noopener noreferrer">
+                                <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clip-rule="evenodd"/>
+                                    <path d="M7.2 8.809H4V19.5h3.2V8.809Z"/>
+                                </svg>
+                            </a>
+                            <!-- Twitter Icon -->
+                            <a href="https://twitter.com/onlineformapro?lang=fr" target="_blank" rel="noopener noreferrer">
+                                <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <path d="M13.795 10.533 20.68 2h-3.073l-5.255 6.517L7.69 2H1l7.806 10.91L1.47 22h3.074l5.705-7.07L15.31 22H22l-8.205-11.467Zm-2.38 2.95L9.97 11.464 4.36 3.627h2.31l4.528 6.317 1.443 2.02 6.018 8.409h-2.31l-4.934-6.89Z"/>
+                                </svg>
+                            </a>
+                            <!-- Facebook Icon -->
+                            <a href="https://www.facebook.com/Onlineformapro/" target="_blank" rel="noopener noreferrer">
+                                <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z" clip-rule="evenodd"/>
+                                </svg>
+                            </a>
+                        </div>
                     </ul>
                 </div>
+                <div class="flex-grow"></div>
+                <div class="mb-4 mr-8 sm:mr-0 sm:mb-0">
+                    <h2 class="mb-2 text-sm font-bold uppercase text-white">Contact</h2>
+                    <ul class="text-gray-400">
+                        <li class="mb-2">19 rue du Praley, 70000 Vesoul</li>
+                        <li class="mb-2">Téléphone :<a href="#" class="hover:text-red-500">+33 (0)3 84 76 52 44</a></li>
+                        <li class="mb-2">E-mail : <a href="#" class="hover:text-red-500">projet@onlineformapro.com</a></li>
+                    </ul>
+                    <img src="./images/etiquette.png" class="h-24" alt="Flowbite Logo" />
+                </div>
             </div>
-           
         </div>
-        <hr class="my-6  sm:mx-auto border-gray-700 lg:my-8" />
-        <span class="block text-sm  sm:text-centertext-gray-400">
+        <hr class="my-6 sm:mx-auto border-gray-700 lg:my-8" />
+        <span class="block text-sm sm:text-center text-gray-400">
             © 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
         </span>
     </div>
